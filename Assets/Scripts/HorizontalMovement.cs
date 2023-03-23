ac4f4cb17a8afc507dbedb031763d45dd14ed945
Anjana Senanayake <sach@Anjanas-MacBook-Pro.local>
2023-03-23 19:38:34 +1100

    fix(horizontalMovement): forgot to add script to commit XD

--- Assets/Scripts/HorizontalMovement.cs
+++ Assets/Scripts/HorizontalMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class HorizontalMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Get input (-1 or 1 for A or D)
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        // Set the velocity of the Rigidbody2D based on the input and move speed
+        rb.velocity = new Vector2(moveHorizontal * moveSpeed, rb.velocity.y);
     }
 }

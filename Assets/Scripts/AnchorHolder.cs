5afeacc775ccf696a05f679f3b1019e5647fa907
Clive Patrick <clive.m.patrick@gmail.com>
2023-03-24 22:33:46 +1100

    feat(new component): Added anchor holder component

---
+++ Assets/Scripts/AnchorHolder.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorHolder : MonoBehaviour
+{
+    public bool HoldingAnchor => m_Anchor != null;
+    public float GrabRadius = 1;
+
+    private Anchor m_Anchor;
+
+    private void OnAnchorInteract()
+    {
+        if (HoldingAnchor)
+        {
+            DropAnchor();
+        }
+        else
+        {
+            GrabAnchor();
+        }
+    }
+
+    private void GrabAnchor()
+    {
+        if (HoldingAnchor)
+        {
+            return;
+        }
+
+        var anchorLayerMask = LayerMask.GetMask("Anchor");
+        var collider = Physics2D.OverlapCircle(transform.position, GrabRadius, anchorLayerMask);
+
+        if (collider == null)
+        {
+            return;
+        }
+
+        m_Anchor = collider.GetComponent<Anchor>();
+
+        if (m_Anchor == null)
+        {
+            return;
+        }
+
+        m_Anchor.transform.SetParent(transform);
+        m_Anchor.transform.localPosition = Vector3.zero;
+        m_Anchor.Simulated = false;
+    }
+
+    private void DropAnchor()
+    {
+        if (!HoldingAnchor)
+        {
+            return;
+        }
+
+        // Set parent of anchor to world and keep its world position
+        m_Anchor.transform.SetParent(null, true);
+        m_Anchor.Simulated = true;
+
+        m_Anchor = null;
+    }
+}

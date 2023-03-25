5c2a1d6f8eaa37f496d6e22699084ead8ecc0e53
Brian Burns <brian.b5829@gmail.com>
2023-03-25 22:58:56 +1100

    feat(grapple surface): Added logic to support anchor states

--- Assets/Scripts/Anchor.cs
+++ Assets/Scripts/Anchor.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AnchorState
+{
+    Free,
+    Grounded,
+    Lodged,
+    Held
+}
+
 public class Anchor : MonoBehaviour
 {
     public bool Simulated { set => m_Rigidbody.simulated = value; }
 
+    private AnchorState m_State;
+    private GameObject m_ObjectLastLodgedIn;
     private Rigidbody2D m_Rigidbody;
 
     private void Start()
@@ -15,12 +25,76 @@ public class Anchor : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+       
+        if (collision.gameObject.CompareTag("Grapplable") && collision.gameObject != m_ObjectLastLodgedIn)
+        {
+            UpdateState(AnchorState.Lodged);
+            m_ObjectLastLodgedIn = collision.gameObject;
+        }
+        else
+        {
+            Debug.Log("updating state to grounded");
+            UpdateState(AnchorState.Grounded);
+        }
         m_Rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 
-    public void Unstick()
+    public void PickUp(Transform parent, Vector2 positionOffset)
     {
-        m_Rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        Debug.Log("Picking up");
+        transform.SetParent(parent);
+        transform.localPosition = positionOffset;
+        UpdateState(AnchorState.Held);
+    }
+
+    private void UpdateState(AnchorState next)
+    {
+
+        //Reset lodge object so that it can be lodged in again
+        if (next == AnchorState.Grounded || next == AnchorState.Held)
+        {
+            m_ObjectLastLodgedIn = null;
+        }
+
+        //set contraints based on state
+        if (next == AnchorState.Free)
+        {
+            m_Rigidbody.constraints = RigidbodyConstraints2D.None;
+        }
+        else
+        {
+            m_Rigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
+        //Reset rotation when anchor is picked up
+        if (next == AnchorState.Held)
+        {
+            Simulated = false;
+            m_Rigidbody.rotation = 0;
+        }
+        else
+        {
+            Simulated = true;
+        }
+
+        m_State = next;
+    }
+
+    public void Drop()
+    {
+        // Set parent of anchor to world and keep its world position
+        transform.SetParent(null, true);
+        UpdateState(AnchorState.Free);
+    }
+
+    public void Dislodge()
+    {
+        UpdateState(AnchorState.Free);
+    }
+
+    public void Lodge()
+    {
+        UpdateState(AnchorState.Lodged);
     }
 
     public void Throw(Vector2 velocity)

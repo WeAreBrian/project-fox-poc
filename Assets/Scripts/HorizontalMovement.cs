4eaac7474078e4c49e3ceae50d0a00b6f585708f
Clive Patrick <clive.m.patrick@gmail.com>
2023-03-26 17:11:53 +1100

    feat(player): Adjusted player movement

--- Assets/Scripts/HorizontalMovement.cs
+++ Assets/Scripts/HorizontalMovement.cs
@@ -35,7 +35,16 @@ public class HorizontalMovement : MonoBehaviour
             return;
         }
 
-        float horizontalAxisValue = directionX;
+        var horizontalAxisValue = directionX;
         rb.velocity = new Vector2(horizontalAxisValue * MoveSpeed, rb.velocity.y);
+
+        //if (m_Grounded.OnGround)
+        //{
+        //    rb.velocity = new Vector2(horizontalAxisValue * MoveSpeed, rb.velocity.y);
+        //}
+        //else
+        //{
+        //    rb.AddForce(new Vector2(directionX * 50.0f, 0));
+        //}
     }
 }

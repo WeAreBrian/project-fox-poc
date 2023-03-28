167482b483faf170c70d657c41f9ad26e059b929
Evita Liu <69669364+emiko1412@users.noreply.github.com>
2023-03-28 18:31:25 +1100

    feat(Pressure plate logic): Created script and added functionality

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+// Pressure plate is an environmental object that is activated when something heavy is placed on it, like the anchor
+public class PressurePlate : MonoBehaviour
+{
+
+    [SerializeField] private bool m_Active; //whether the pressure plate is active or not
+    [SerializeField] private float m_ActivationMass; //the mass needed to activate pressure plate
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // No foxes can activate this pressure plate
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        // check if object has a RigidBody
+        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (rb == null) { 
+            return; 
+        }
+
+        // and if it's mass is equal or higher than activation mass,
+        // then activate the pressure plate
+        if (rb.mass >= m_ActivationMass)
+        {
+            m_Active = true;
+        }
+    }
+
+    // Same logic as OnCollisionEnter2D
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            return;
+        }
+
+        if (rb.mass >= m_ActivationMass)
+        {
+            m_Active = false;
+        }
+    }
+}

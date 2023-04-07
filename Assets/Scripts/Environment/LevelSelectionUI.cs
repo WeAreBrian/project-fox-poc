b53864fd0bcc412b2d87e1417edda29ff67b0278
ShiroKatake <tgkhang262@gmail.com>
2023-04-07 14:55:47 +1000

    feat: scene selector button

--- Assets/Scripts/Environment/LevelSelectionUI.cs
+++ Assets/Scripts/Environment/LevelSelectionUI.cs
@@ -11,11 +11,12 @@ public class LevelSelectionUI : MonoBehaviour
         for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
         {
 			EditorBuildSettingsScene scene = EditorBuildSettings.scenes[i];
-			if (scene.enabled)
+			if (scene.enabled && !scene.path.Contains("LevelSelection"))
             {
 				LevelSelector levelSelector = Instantiate(m_levelSelectorPrefab, gameObject.transform);
-				levelSelector.Initialize(i, System.IO.Path.GetFileNameWithoutExtension(scene.path));
-			}
+				//levelSelector.Initialize(i, i.ToString());    Uncomment this code when it comes to actual build so only level number is displayed
+                levelSelector.Initialize(i, System.IO.Path.GetFileNameWithoutExtension(scene.path));
+            }
         }
 	}
 }

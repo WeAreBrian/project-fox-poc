4aa395783a6c73bf70a632f643ed74c77e05d420
ShiroKatake <tgkhang262@gmail.com>
2023-04-09 16:33:47 +1000

    fix(return to level selection): fixed event system warning

--- Assets/Scripts/Environment/ReturnToLevelSelect.cs
+++ Assets/Scripts/Environment/ReturnToLevelSelect.cs
@@ -10,33 +10,38 @@ public class ReturnToLevelSelect : MonoBehaviour
     [SerializeField]
     private GameObject m_eventSystem;
 
-    public static ReturnToLevelSelect m_instance = null;
+    public static ReturnToLevelSelect m_instance;
 
     private void Awake()
     {
-        SceneManager.sceneLoaded += OnSceneLoaded;
         if (m_instance != null && m_instance != this)
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
         else
         {
             m_instance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
-        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode _)
     {
+        if (m_instance == this)
+        {
+            if (scene.buildIndex != 0)
+                m_returnButton.SetActive(true);
+
+            // This check will be redundant later when every level has UI and a respective EventSystem
+            if (FindObjectOfType<EventSystem>() == null)
+                m_eventSystem.SetActive(true);
+        }
+    }
+
+    public void ResetButton() {
         m_returnButton.SetActive(false);
         m_eventSystem.SetActive(false);
-
-        if (scene.buildIndex != 0)
-            m_returnButton.SetActive(true);
-
-        // This check will be redundant later when every level has UI and a respective EventSystem
-        if (FindObjectOfType<EventSystem>() == null)
-            m_eventSystem.SetActive(true);
     }
 }

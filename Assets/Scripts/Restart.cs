2e5e2678772666eb0a0116b8eb1b43880dcb157d
jess-rubino <102111467@student.swin.edu.au>
2023-04-09 15:09:46 +1000

    feat(): added in a comment

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// will possibly convert this script into a singleton - potentially merging with return to level select
 public class Restart : MonoBehaviour
 {
    void Update()

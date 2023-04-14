eac0fb4fc6a7f397e2752394da8b828c7d88a41d
Clive Patrick <clive.m.patrick@gmail.com>
2023-04-15 01:05:53 +1000

    feat(cannon): Added projectile solver

---
+++ Assets/Scripts/ProjectileSolver.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using static UnityEngine.UI.Image;
+using UnityEngine.UIElements;
+
+public static class ProjectileSolver
+{
+	/// <summary>
+	/// Find the angle required to hit the target.
+	/// </summary>
+	/// <param name="position">The starting position of the projectile.</param>
+	/// <param name="target">The target position of the projectile.</param>
+	/// <param name="speed">The magnitude of the initial velocity.</param>
+	/// <param name="angle">The angle of the initial velocity.</param>
+	public static bool CalculateAngle(Vector2 position, Vector2 target, float speed, out float angle)
+	{
+		var displacement = target - position;
+		var horizontalDistance = displacement.x;
+		var verticalDistance = displacement.y;
+		var horizontalSpeed = speed;
+		var gravity = Physics2D.gravity.y;
+
+		// Calculate the initial vertical speed needed to hit the target
+		var a = -0.5f * gravity;
+		var b = verticalDistance;
+		var c = -horizontalDistance;
+		var discriminant = b * b - 4f * a * c;
+
+		if (discriminant < 0)
+		{
+			angle = 0;
+			return false;
+		}
+
+		var root = Mathf.Sqrt(discriminant);
+		var timeToTarget = Mathf.Max((-b + root) / (2f * a), (-b - root) / (2f * a));
+		var verticalSpeed = -gravity * timeToTarget;
+
+		// Calculate the firing angle
+		angle = Mathf.Atan2(verticalSpeed, horizontalSpeed) * Mathf.Rad2Deg;
+
+		return true;
+	}
+}

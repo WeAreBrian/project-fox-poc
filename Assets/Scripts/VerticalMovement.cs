dcfb91236e542126f59b268bb470af4b4c64047c
ShiroKatake <tgkhang262@gmail.com>
2023-04-17 13:16:21 +1000

    feat(jumping): added coyote time

--- Assets/Scripts/VerticalMovement.cs
+++ Assets/Scripts/VerticalMovement.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VerticalMovement : MonoBehaviour
 {
     public float JumpForce;
-
     public float JumpCoefficient = 1;
 
+    [SerializeField]
+    private float m_coyoteTime;
     private Rigidbody2D m_RigidBody;
     private Grounded m_Grounded;
+    private float m_coyoteTimeCounter;
     private AnchorThrower m_Thrower;
 
     private void Awake()
@@ -19,9 +19,21 @@ public class VerticalMovement : MonoBehaviour
         m_Thrower = GetComponent<AnchorThrower>();
     }
 
+    private void Update()
+    {
+        if (!m_Grounded.OnGround)
+        {
+            m_coyoteTimeCounter += Time.deltaTime;
+        }
+        else
+        {
+            m_coyoteTimeCounter = 0;
+        }
+    }
+
     private void OnJump()
     {
-        if (m_Grounded.OnGround && !m_Thrower.WindingUp)
+        if ((m_Grounded.OnGround || m_coyoteTimeCounter < m_coyoteTime) && !m_Thrower.WindingUp)
         {
             m_RigidBody.velocity = new Vector2(m_RigidBody.velocity.x, JumpForce*JumpCoefficient);
         }

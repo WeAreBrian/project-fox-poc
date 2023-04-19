9b90729385de7fcb3e63a22a5727077639cbcea4
Anjana Senanayake <sach@Anjanas-MacBook-Pro.local>
2023-04-20 01:29:26 +1000

    fix(scripts): changed water to use inbuilt buoyancy, and scripts account for player holding anchor properly

--- Assets/Scripts/WaterVolume.cs
+++ Assets/Scripts/WaterVolume.cs
@@ -4,17 +4,96 @@ using UnityEngine;
 
 public class WaterVolume : MonoBehaviour
 {
+    [Header("Player Holding Anchor Only Variables")]
+    public float WaterDensityForPlayerHoldingAnchor;
+	public float LinearDragForPlayerHoldingAnchor;
+	public float AngularDragForPlayerHoldingAnchor;
+
+    [Header("Flow variables which affect children too (EDIT THESE)")]
+    public float m_FlowAngle;
+    public float m_FlowMagnitude;
+    public float m_FlowVariation;
+
+    private GameObject m_PlayerFox;
+	private BuoyancyEffector2D m_PlayerBuoyancyEffector2d;
+	private float m_BaseWaterDensity;
+	private BuoyancyEffector2D m_ParentBuoyancyEffector2d;
+
+	private void Start()
+	{
+		m_PlayerFox = GameObject.Find("PlayerFox");
+		//get the buoyancy component in the children that is for the player
+		m_PlayerBuoyancyEffector2d = gameObject.transform.Find("WaterVolumeForPlayer").gameObject.GetComponent<BuoyancyEffector2D>();
+		m_ParentBuoyancyEffector2d = GetComponent<BuoyancyEffector2D>();
+
+		m_ParentBuoyancyEffector2d.flowAngle = m_FlowAngle;
+		m_ParentBuoyancyEffector2d.flowMagnitude = m_FlowMagnitude;
+		m_ParentBuoyancyEffector2d.flowVariation = m_FlowVariation;
+
+		//set flow vars for each child
+        foreach (Transform childTransform in transform)
+        {
+			BuoyancyEffector2D buoyancyEffector2D = childTransform.GetComponent<BuoyancyEffector2D>();
+            if (buoyancyEffector2D != null)
+            {
+				buoyancyEffector2D.flowAngle = m_FlowAngle;
+				buoyancyEffector2D.flowMagnitude = m_FlowMagnitude;
+				buoyancyEffector2D.flowVariation = m_FlowVariation;
+            }
+        }
+    }
+
+	private void FixedUpdate()
+	{
+		if (m_PlayerFox.GetComponent<AnchorHolder>().HoldingAnchor)
+		{
+            m_PlayerBuoyancyEffector2d.density = WaterDensityForPlayerHoldingAnchor;
+            m_PlayerBuoyancyEffector2d.surfaceLevel = m_ParentBuoyancyEffector2d.surfaceLevel;
+			m_PlayerBuoyancyEffector2d.linearDrag = LinearDragForPlayerHoldingAnchor;
+            m_PlayerBuoyancyEffector2d.angularDrag = AngularDragForPlayerHoldingAnchor;
+
+            m_PlayerBuoyancyEffector2d.flowAngle = m_ParentBuoyancyEffector2d.flowAngle;
+            m_PlayerBuoyancyEffector2d.flowMagnitude = m_ParentBuoyancyEffector2d.flowMagnitude;
+            m_PlayerBuoyancyEffector2d.flowVariation = m_ParentBuoyancyEffector2d.flowVariation;
+        }
+		else
+        {
+            m_PlayerBuoyancyEffector2d.density = m_ParentBuoyancyEffector2d.density;
+            m_PlayerBuoyancyEffector2d.surfaceLevel = m_ParentBuoyancyEffector2d.surfaceLevel;
+            m_PlayerBuoyancyEffector2d.linearDrag = m_ParentBuoyancyEffector2d.linearDrag;
+            m_PlayerBuoyancyEffector2d.angularDrag = m_ParentBuoyancyEffector2d.angularDrag;
+            m_PlayerBuoyancyEffector2d.flowAngle = m_ParentBuoyancyEffector2d.flowAngle;
+            m_PlayerBuoyancyEffector2d.flowMagnitude = m_ParentBuoyancyEffector2d.flowMagnitude;
+            m_PlayerBuoyancyEffector2d.flowVariation = m_ParentBuoyancyEffector2d.flowVariation;
+        }
+	}
+}
+
+
+
+
+//OLD CODE FOR BUOYANCY BEFORE I KNEW THERE WAS A BUOYANCY COMPONENT
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterVolume : MonoBehaviour
+{
 	public float m_WaterDrag = 0.7f;
 	public float m_BouyancyForce = 14.0f;
 	private List<GameObject> m_IgnoredObjects = new List<GameObject>();
 	private List<Rigidbody2D> buoyantObjects = new List<Rigidbody2D>();
 	private float m_FakeMassMultiplierForPlayerHoldingAnchor = 11f; //11 cos fox is 10mass and anchor is 100 so 10*11 = 10+100
+	private BoxCollider2D m_Collider;
 
 	private void Start()
 	{
 		// Subscribe to the LinksCreated event in the Chain class
 		Chain linksCreator = FindObjectOfType<Chain>();
 		linksCreator.LinksCreated += OnLinksCreated;
+
+		m_Collider = GetComponent<BoxCollider2D>();
 	}
 
 	private void OnLinksCreated()
@@ -104,10 +183,42 @@ public class WaterVolume : MonoBehaviour
 
 		// Apply buoyancy force
 		Vector2 buoyantForce = new Vector2(0.0f, displacedWaterWeight);
-		submergedRigidbody.AddForce(buoyantForce);
+		submergedRigidbody.AddForce(buoyantForce * GetSubmersionPercentage(collider, m_Collider));
+		Debug.Log(GetSubmersionPercentage(collider, m_Collider));
 
 		// Apply drag force (like air resistance, slows things down when move, faster they move, the greater the resistance)
 		Vector2 dragForce = -submergedRigidbody.velocity * m_BouyancyForce * m_WaterDrag;
 		submergedRigidbody.AddForce(dragForce);
 	}
+
+
+	public float GetSubmersionPercentage(Collider2D collider, Collider2D waterTrigger)
+	{
+		// Get the bounds of the collider and water trigger
+		Bounds colliderBounds = collider.bounds;
+		Bounds waterBounds = waterTrigger.bounds;
+
+		// Calculate the area of intersection between the two bounds
+		float intersectionArea = Mathf.Max(0f, Mathf.Min(colliderBounds.max.x, waterBounds.max.x) - Mathf.Max(colliderBounds.min.x, waterBounds.min.x))
+							   * Mathf.Max(0f, Mathf.Min(colliderBounds.max.y, waterBounds.max.y) - Mathf.Max(colliderBounds.min.y, waterBounds.min.y));
+
+		// Calculate the area of the collider
+		float colliderArea = colliderBounds.size.x * colliderBounds.size.y;
+
+		// Calculate the submersion percentage
+		float submersionPercentage = intersectionArea / colliderArea;
+
+		return submersionPercentage;
+	}
+
+
+
+
+
+
+
+
+
 }
+
+*/

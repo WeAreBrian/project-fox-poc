5ccaab671757a8dc7c009f1ea3e4898d8559974d
Evita Liu <69669364+emiko1412@users.noreply.github.com>
2023-04-20 16:16:38 +1000

    feat(fox jump): added downward force when releasing jump

--- Assets/Scripts/VerticalMovement.cs
+++ Assets/Scripts/VerticalMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VerticalMovement : MonoBehaviour
 {
@@ -7,10 +8,18 @@ public class VerticalMovement : MonoBehaviour
 
     [SerializeField]
     private float m_coyoteTime;
+
+    [SerializeField]
+    private bool m_debug;
+
     private Rigidbody2D m_RigidBody;
     private Grounded m_Grounded;
     private float m_coyoteTimeCounter;
     private AnchorThrower m_Thrower;
+
+    [SerializeField]
+    private float m_releaseJumpDownForce;
+    private bool m_jumpDown;
 
     private void Awake()
     {
@@ -24,18 +33,43 @@ public class VerticalMovement : MonoBehaviour
         if (!m_Grounded.OnGround)
         {
             m_coyoteTimeCounter += Time.deltaTime;
-        }
+			
+		}
         else
         {
-            m_coyoteTimeCounter = 0;
-        }
+			m_coyoteTimeCounter = 0;
+		}
     }
 
-    private void OnJump()
+    private void FixedUpdate()
     {
-        if ((m_Grounded.OnGround || m_coyoteTimeCounter < m_coyoteTime) && !m_Thrower.WindingUp)
+		if (!m_Grounded.OnGround && m_jumpDown)
+		{
+            m_RigidBody.AddForce(Vector2.down * m_releaseJumpDownForce);
+
+		}
+		else
+		{
+            m_jumpDown = false;
+		}
+	}
+
+    private void OnJump(InputValue value)
+    {
+		
+		if (value.Get<float>() == 1 && (m_Grounded.OnGround || m_coyoteTimeCounter < m_coyoteTime) && !m_Thrower.WindingUp)
         {
+            if (m_debug) Debug.Log("Jump pressed");
             m_RigidBody.velocity = new Vector2(m_RigidBody.velocity.x, JumpForce*JumpCoefficient);
+
+			return;
         }
+
+		if (value.Get<float>() == 0 && !m_Grounded.OnGround && m_RigidBody.velocity.y > 0.01f)
+        {
+			if (m_debug) Debug.Log("Jump down");
+            m_jumpDown = true;
+		}
     }
+
 }

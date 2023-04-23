34cacaafb98db1f133cb7048d2bfc8713af4bf06
Evita Liu <69669364+emiko1412@users.noreply.github.com>
2023-04-23 10:08:39 +1000

    feat(collider script): added offset

--- Assets/Scripts/PlatformColliderAdjustment.cs
+++ Assets/Scripts/PlatformColliderAdjustment.cs
@@ -7,7 +7,7 @@ public class PlatformColliderAdjustment : MonoBehaviour
     private BoxCollider2D m_boxCollider;
     private SpriteRenderer m_spriteRenderer;
     [SerializeField]
-    private float m_offset;
+    private float m_sizeOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,7 @@ public class PlatformColliderAdjustment : MonoBehaviour
         m_boxCollider = GetComponent<BoxCollider2D>();
         m_spriteRenderer = GetComponent<SpriteRenderer>();
 
-        m_boxCollider.size = new Vector2(m_spriteRenderer.size.x + m_offset, m_spriteRenderer.size.y + m_offset);
+        m_boxCollider.size = new Vector2(m_spriteRenderer.size.x + m_sizeOffset, m_spriteRenderer.size.y + m_sizeOffset);
     }
 
     // Update is called once per frame

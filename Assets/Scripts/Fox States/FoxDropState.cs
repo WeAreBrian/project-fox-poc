fe5f2f6a809d44f9dfa956e697fae96baaa72386
Evita Liu <69669364+emiko1412@users.noreply.github.com>
2023-05-08 18:42:25 +1000

    feat(drop state): state transitions automatically

--- Assets/Scripts/Fox States/FoxDropState.cs
+++ Assets/Scripts/Fox States/FoxDropState.cs
@@ -17,10 +17,10 @@ public class FoxDropState : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("isDropping", false);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)

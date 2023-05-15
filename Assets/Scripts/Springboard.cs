6f8b18ebc739dc0441ba26ab238bdc4401ce14da
Anjana Senanayake <58893281+sachdawg@users.noreply.github.com>
2023-05-15 21:57:59 +1000

    stuff

--- Assets/Scripts/Springboard.cs
+++ Assets/Scripts/Springboard.cs
@@ -7,15 +7,33 @@ public class Springboard : MonoBehaviour
 {
     [SerializeField]
     private float m_SpringForce = 120f;
+    private GameObject m_ChainRigidBodies;
 
+    void Start(){
+        //Get the rigidbodys in chain in array
+        Rigidbody2D[] m_ChainRigidBodiesArray = GameObject.Find("PhysicsChain").GetComponentsInChildren<Rigidbody2D>();
+    }
     void OnTriggerEnter2D(Collider2D m_Collision)
     {
+        
+
         //  AudioController.PlaySound(m_ActivateSound, 1, 1, MixerGroup.SFX);
         Rigidbody2D m_OtherRigidBody = m_Collision.GetComponent<Rigidbody2D>();
-        if (m_OtherRigidBody != null)
+        if (m_OtherRigidBody != null && IsRigidBodyIsAChainRigidBody(m_ChainRigidBodies))
         {
             m_OtherRigidBody.AddForce(m_SpringForce * Vector2.up, ForceMode2D.Impulse);
         }
     }
 
+    bool IsRigidBodyIsAChainRigidBody(Rigidbody2D m_RBToCheck){
+        foreach (Rigidbody2D rb in m_ChainRigidBodies)
+        {
+            if (rb == m_RBToCheck){
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+//I was looking at unity debug log

10b154791b83c0dd6568a4c5824eef22766dabaf
PorridgeB <49393032+PorridgeB@users.noreply.github.com>
2023-05-19 15:29:39 +1000

    added assets to scene

---
+++ Assets/Scripts/LayerParallax.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerParallax : MonoBehaviour
+{
+    public Transform ReferenceTransform;
+    public float SpeedMultiplier;
+    public PositionDelta camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.GetComponent<PositionDelta>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //skip first frame because it seems to take the position from origin as the cameras delta for the first frame
+        if (Time.frameCount == 1) return;
+
+        transform.position += camera.Delta * (SpeedMultiplier);
+    }
+}

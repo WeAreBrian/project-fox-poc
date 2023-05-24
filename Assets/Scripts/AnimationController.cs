4a6245d01968917c5dbcbcebd9f03b5390dd7e67
Brian Burns <brian.b5829@gmail.com>
2023-05-24 19:15:12 +1000

    Finished Fox 3D character controller

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -7,10 +7,14 @@ public class AnimationController : MonoBehaviour
 {
     private Animator m_Animator;
     private Grounded m_Grounded;
+    private AnchorHolder m_AnchorHolder;
+    private ChainMovement m_ChainMovement;
     [SerializeField] bool m_debugInfo;
     private Rigidbody2D m_RigidBody;
     private GameObject m_model;
-
+    private float k_WalkSpeedScale = 0.3f;
+    private float k_ClimbSpeedScale = 0.5f;
+    private bool m_Moving;
 
     private Camera m_cam;
 
@@ -21,29 +25,56 @@ public class AnimationController : MonoBehaviour
         m_Grounded = GetComponent<Grounded>();
         m_RigidBody = GetComponent<Rigidbody2D>();
         m_model = GetComponentInChildren<Animator>().gameObject;
+        m_AnchorHolder = GetComponent<AnchorHolder>();
+        m_ChainMovement = GetComponent<ChainMovement>();
         m_cam = Camera.main;
+    }
+
+    private void Update()
+    {
+
+
+        m_Animator.SetBool("Grounded", m_Grounded.OnGround);
+        m_Animator.SetBool("HoldingAnchor", m_AnchorHolder.HoldingAnchor);
+        m_Animator.SetBool("Climbing", m_ChainMovement.Mounted);
+        if (m_Grounded.OnGround && m_Moving)
+        {
+            m_Animator.speed = Mathf.Abs(m_RigidBody.velocity.x) * k_WalkSpeedScale;
+        }
+        else if (m_ChainMovement.Mounted)
+        {
+            m_Animator.speed = Mathf.Abs(m_RigidBody.velocity.y);
+        }
+        else
+        {
+            m_Animator.speed = 1;
+        }
     }
 
     void OnMove(InputValue value)
     {
         // going left, flip fox rotation to face left
-        if (value.Get<float>() == -1f)
+        if (value.Get<float>() < 0f)
         {
-            m_model.transform.LookAt(transform.position+Vector3.left);
+
+            m_model.transform.rotation = Quaternion.Euler(0, -90, 0);
         }
 
         // going right, flip fox rotation to face right
-        else if (value.Get<float>() == 1f)
+        else if (value.Get<float>() > 0f)
         {
-            m_model.transform.LookAt(transform.position + Vector3.right);
+
+            m_model.transform.rotation = Quaternion.Euler(0, 90, 0);
         }
 
         else
         {
-            m_model.transform.rotation = new Quaternion(0, 180, 0, 0);
+            m_model.transform.rotation = Quaternion.Euler(0, 180, 0);
         }
 
-        m_Animator.SetBool("IsMoving", value.Get<float>() != 0);
+        m_Moving = value.Get<float>() != 0;
+
+        m_Animator.SetBool("IsMoving", m_Moving);
     }
 
 

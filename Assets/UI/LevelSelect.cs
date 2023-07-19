4904a44692ea76e842b631dac39f9ebb2b97437f
Anjana <58893281+sachdawg@users.noreply.github.com>
2023-07-19 16:41:45 +1000

    added some code to make level buttons take u to a scene

--- Assets/UI/LevelSelect.cs
+++ Assets/UI/LevelSelect.cs
@@ -7,14 +7,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-    VisualElement root;
+    [SerializeField]
+    private string m_Level1;
+    [SerializeField]
+    private string m_Level2;
+    [SerializeField]
+    private string m_Level3;
+    private VisualElement root;
+
     private void OnEnable()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         
-        
-        Button m_PlayButton = root.Q<Button>("Play");
-        m_PlayButton.clicked += () => SceneManager.LoadScene("A");
-        
+        //Get buttons
+        Button m_Level1Button = root.Q<Button>("Level1");
+        Button m_Level2Button = root.Q<Button>("Level2");
+        Button m_Level3Button = root.Q<Button>("Level3");
+
+        //Assign event for the buttons
+        m_Level1Button.clicked += () => SceneManager.LoadScene(m_Level1);
+        m_Level1Button.clicked += () => SceneManager.LoadScene(m_Level2);
+        m_Level1Button.clicked += () => SceneManager.LoadScene(m_Level3);
+
     }
 }

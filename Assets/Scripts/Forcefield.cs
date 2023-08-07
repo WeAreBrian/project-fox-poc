5d2cf357fba0f8bb31037849bc7ccc6045649546
Brian Burns <brian.b5829@gmail.com>
2023-08-07 20:49:26 +1000

    broke surfing, fixed surfing

--- Assets/Scripts/Forcefield.cs
+++ Assets/Scripts/Forcefield.cs
@@ -6,7 +6,7 @@ public class Forcefield : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             collision.gameObject.GetComponent<AnchorHolder>().DropAnchor();
         }

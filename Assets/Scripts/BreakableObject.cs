8aa07363b0eb4f31c1d19d7ad799daa6927b27db
jess-rubino <102111467@student.swin.edu.au>
2023-08-08 16:00:19 +1000

    feat: finished working on the particle spray direction

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class BreakableObject : MonoBehaviour
 {
-    [SerializeField] 
-    private GameObject m_brokenWallPieces; 
+    [SerializeField]
+    private GameObject m_brokenWallPieces;
 
-    [SerializeField] 
+    [SerializeField]
     private GameObject m_smokeParticles;
 
+    [SerializeField]
+    private bool m_isVertical;
+
+    UnityEngine.Quaternion GetParticleSprayDirection(Collider2D collision)
+    {
+        UnityEngine.Vector2 m_localPos = gameObject.transform.position - collision.gameObject.transform.position;
+        if (m_localPos.x > 0 && !m_isVertical) {
+            return UnityEngine.Quaternion.Euler(-90, 0, 0);
+        }
+
+        else if (m_localPos.y > 0 && m_isVertical) {
+            return UnityEngine.Quaternion.Euler(0, -90, 0);
+        }
+        else {
+            return UnityEngine.Quaternion.identity;
+        }
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Anchor"))
-    	{
-            Instantiate(m_brokenWallPieces, transform.position, transform.rotation);
-            // set the y position to be whatever direction you're throwing the anchor in
-            Instantiate(m_smokeParticles, transform.position, transform.rotation);
-            Destroy(gameObject); 
+        {
+            Instantiate(m_brokenWallPieces, transform.position, GetParticleSprayDirection(collision));
+            Instantiate(m_smokeParticles, transform.position, GetParticleSprayDirection(collision));
+            Destroy(gameObject);
         }
     }
 }

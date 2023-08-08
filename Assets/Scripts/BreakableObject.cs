9accc1c48e8b96073de073759c15135f585a47f5
jess-rubino <102111467@student.swin.edu.au>
2023-08-08 16:16:54 +1000

    fix: removed using System.Numerics as it was causing weird issues for vector and quaternion

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 public class BreakableObject : MonoBehaviour
@@ -14,18 +13,18 @@ public class BreakableObject : MonoBehaviour
     [SerializeField]
     private bool m_isVertical;
 
-    UnityEngine.Quaternion GetParticleSprayDirection(Collider2D collision)
+    Quaternion GetParticleSprayDirection(Collider2D collision)
     {
-        UnityEngine.Vector2 m_localPos = gameObject.transform.position - collision.gameObject.transform.position;
+        Vector2 m_localPos = gameObject.transform.position - collision.gameObject.transform.position;
         if (m_localPos.x > 0 && !m_isVertical) {
-            return UnityEngine.Quaternion.Euler(-90, 0, 0);
+            return Quaternion.Euler(-90, 0, 0);
         }
 
         else if (m_localPos.y > 0 && m_isVertical) {
-            return UnityEngine.Quaternion.Euler(0, -90, 0);
+            return Quaternion.Euler(0, -90, 0);
         }
         else {
-            return UnityEngine.Quaternion.identity;
+            return Quaternion.identity;
         }
     }
     void OnTriggerEnter2D(Collider2D collision)

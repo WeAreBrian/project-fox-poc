11ba9c113439af95641b63ec303c988e7f9e3956
jess-rubino <102111467@student.swin.edu.au>
2023-08-08 14:25:57 +1000

    feat: changed the broken pieces to rubble particles and added smoke particles

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class BreakableObject : MonoBehaviour
 {
-    public GameObject m_brokenWallPieces; 
+    [SerializeField] 
+    private GameObject m_brokenWallPieces; 
+
+    [SerializeField] 
+    private GameObject m_smokeParticles;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Anchor"))
     	{
             Instantiate(m_brokenWallPieces, transform.position, transform.rotation);
+            // set the y position to be whatever direction you're throwing the anchor in
+            Instantiate(m_smokeParticles, transform.position, transform.rotation);
             Destroy(gameObject); 
         }
     }

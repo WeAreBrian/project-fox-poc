2c3d78a3aa7724424f45ffb618ca9b1762dac3d5
PorridgeB <49393032+PorridgeB@users.noreply.github.com>
2023-08-10 16:19:32 +1000

    added recall cooldown (fox must land to refresh)

--- Assets/Scripts/Recall.cs
+++ Assets/Scripts/Recall.cs
@@ -11,7 +11,14 @@ public class Recall : MonoBehaviour
 
 	private Anchor m_Anchor;
 	private AnchorHolder m_Holder;
+	private Grounded m_Grounded;
 
+
+	[SerializeField]
+	private Color m_CooldownColour;
+	private Material m_AnchorMaterial;
+
+	private bool OnCooldown => m_AnchorMaterial.color == m_CooldownColour;
 
     [SerializeField]
 	private AudioClip m_RecallSound;
@@ -20,15 +27,27 @@ public class Recall : MonoBehaviour
 	{
 		m_Anchor = FindObjectOfType<Anchor>();
 		m_Holder = GetComponent<AnchorHolder>();
+		m_Grounded = GetComponent<Grounded>();
+		m_Grounded.Landed.AddListener(ResetRecall);
+		m_AnchorMaterial = m_Anchor.GetComponentInChildren<MeshRenderer>().material;
 
-    }
+	}
 
 
 
     private void OnRecall()
 	{
-		AudioController.PlaySound(m_RecallSound, 1, 1, MixerGroup.SFX);
-		m_Anchor.transform.position = transform.position;
-		m_Holder.ForcePickup();
+		if (!OnCooldown && !m_Holder.HoldingAnchor)
+        {
+			AudioController.PlaySound(m_RecallSound, 1, 1, MixerGroup.SFX);
+			m_Anchor.transform.position = transform.position;
+			m_Holder.ForcePickup();
+			m_AnchorMaterial.color = m_CooldownColour;
+		}
+	}
+
+	private void ResetRecall()
+    {
+		m_AnchorMaterial.color = Color.white;
 	}
 }

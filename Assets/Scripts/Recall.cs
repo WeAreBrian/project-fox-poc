ac1d835b835b1f78bd6afdaedf58b38de144d8df
harley102 <54125520+harley102@users.noreply.github.com>
2023-08-22 14:32:18 +1000

    Revert "Merge branch 'main' into 184---decolevel-1"
    This reverts commit cfc7f07a3d5b190d6954d2871b42607aff1422f6, reversing
    changes made to 9f4338c5e2e9a5f3ff3f691fd4610269f169e1aa.

--- Assets/Scripts/Recall.cs
+++ Assets/Scripts/Recall.cs
@@ -21,7 +21,7 @@ public class Recall : MonoBehaviour
 
 	private bool OnCooldown => m_AnchorMaterial.color == m_CooldownColour;
 
-	[SerializeField]
+    [SerializeField]
 	private AudioClip m_RecallSound;
 
 	private void Awake()
@@ -33,38 +33,27 @@ public class Recall : MonoBehaviour
 		m_Grounded = GetComponent<Grounded>();
 		m_Grounded.Landed.AddListener(ResetRecall);
 		m_AnchorMaterial = m_Anchor.GetComponentInChildren<MeshRenderer>().material;
-		m_Anchor.StateChanged.AddListener(AnchorStateChange);
+
 	}
 
 
 
-	private void OnRecall()
+    private void OnRecall()
 	{
 		if (!OnCooldown && !m_Holder.HoldingAnchor)
-		{
+        {
 			//Need to activate the slingshot BEFORE the anchor moves!
-			m_RecallSlingshot?.TrySlingshot();
+        	m_RecallSlingshot?.TrySlingshot();
 
 			AudioController.PlaySound(m_RecallSound, 1, 1, MixerGroup.SFX);
 			m_Anchor.transform.position = transform.position;
 			m_Holder.ForcePickup();
-			if (!m_Grounded.OnGround)
-			{
-				m_AnchorMaterial.color = m_CooldownColour;
-			}
-		}
-	}
-
-	private void AnchorStateChange(AnchorState state)
-	{
-		if (state == AnchorState.Lodged)
-		{
-			ResetRecall();
+			m_AnchorMaterial.color = m_CooldownColour;
 		}
 	}
 
 	private void ResetRecall()
-	{
+    {
 		m_AnchorMaterial.color = Color.white;
 	}
 }

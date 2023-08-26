8aecb9490b406cf5dcc7dd7b7157f374fe284168
Anjana <58893281+sachdawg@users.noreply.github.com>
2023-08-26 11:58:55 +1000

    feat(transition): spikes trigger the transitions

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -5,11 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Spike : MonoBehaviour
 {
+    CloseOrOpenCircle m_HoleTransition;
+
+    private void Start()
+    {
+        m_HoleTransition = GameObject.Find("HoleTransition").GetComponent<CloseOrOpenCircle>();
+        m_HoleTransition.OnShrinkComplete += OnShrinkCompleteCallback;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            StartCoroutine(m_HoleTransition.ShrinkParentObject());
+
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+    }
+
+    private void OnShrinkCompleteCallback()
+    {
+        Debug.Log("Shrink operation in CloseOrOpenCircle is complete.");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    private void OnDisable()
+    {
+        m_HoleTransition.OnShrinkComplete -= OnShrinkCompleteCallback;
+
     }
 }

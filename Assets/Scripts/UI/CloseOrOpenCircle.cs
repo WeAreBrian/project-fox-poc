c847ad218ff156f23b94a003d5acc2c27238892b
Anjana <58893281+sachdawg@users.noreply.github.com>
2023-08-26 11:58:36 +1000

    feat(transition): grow script

--- Assets/Scripts/UI/CloseOrOpenCircle.cs
+++ Assets/Scripts/UI/CloseOrOpenCircle.cs
@@ -1,20 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class CloseOrOpenCircle : MonoBehaviour
 {
-    public float m_ShrinkSpeed = 30f; // The speed at which the parent will shrink
-    public KeyCode m_TriggerKey = KeyCode.Space; // The key that will trigger the shrink effect
+    public float m_Speed = 70f; // The speed at which the parent will shrink or grow
+    public KeyCode m_ShrinkKey = KeyCode.Space; // The key that will trigger the shrink effect
+    public KeyCode m_GrowKey = KeyCode.G; // The key that will trigger the grow effect
 
     private Transform m_ChildTransform;
     private Vector3 m_ChildInitialWorldScale;
+    private GameObject m_PlayerFox;
+    private RectTransform m_RectTransform;
 
-    // Start is called before the first frame update
+    public Action OnShrinkComplete;
+
+    private void Awake()
+    {
+        m_PlayerFox = GameObject.Find("PlayerFox");
+        m_RectTransform = GetComponent<RectTransform>();
+    }
+
     void Start()
     {
+
         if (transform.childCount == 0)
         {
             Debug.LogWarning("This GameObject has no child. The script won't do anything.");
@@ -23,25 +34,41 @@ public class CloseOrOpenCircle : MonoBehaviour
 
         // Assuming the first child is the one to keep the same relative scale
         m_ChildTransform = transform.GetChild(0);
-        m_ChildInitialWorldScale = m_ChildTransform.lossyScale * 2;
+        m_ChildInitialWorldScale = m_ChildTransform.lossyScale;
+
+        StartCoroutine(GrowParentObject());
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Trigger the shrinking effect when the specified key is pressed
-        if (Input.GetKeyDown(m_TriggerKey))
+        if (Input.GetKeyDown(m_ShrinkKey))
         {
             StartCoroutine(ShrinkParentObject());
         }
+
+        // Trigger the growing effect when the specified key is pressed
+        if (Input.GetKeyDown(m_GrowKey))
+        {
+            StartCoroutine(GrowParentObject());
+        }
+
+        
+        
     }
 
     public IEnumerator ShrinkParentObject()
     {
-        while (transform.localScale.x > 0.01f)
+        
+
+        while (transform.localScale.x > 0.001f)
         {
             // Shrink the parent
-            transform.localScale -= Vector3.one * Time.deltaTime * m_ShrinkSpeed;
+            transform.localScale -= Vector3.one * Time.deltaTime * m_Speed;
 
             // Calculate the new local scale for the child to maintain its world scale
             Vector3 newLocalScale = new Vector3(
@@ -53,6 +80,8 @@ public class CloseOrOpenCircle : MonoBehaviour
             // Apply the new local scale to the child
             m_ChildTransform.localScale = newLocalScale;
 
+            KeepCentredOnFox();
+
             yield return null;
         }
 
@@ -62,7 +91,56 @@ public class CloseOrOpenCircle : MonoBehaviour
         // Keep child's world scale intact when parent is at zero
         m_ChildTransform.localScale = m_ChildInitialWorldScale / 0.001f;
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
+        OnShrinkComplete?.Invoke(); // Call the callback
+        Debug.Log("Fin");
+    }
+
+    public IEnumerator GrowParentObject()
+    {
+        
+
+        // Start from a very small scale
+        transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+
+        // Initial scale for the child to maintain its world scale
+        m_ChildTransform.localScale = m_ChildInitialWorldScale / 0.001f;
+
+        while (transform.localScale.x < 25.0f)
+        {
+            // Grow the parent
+            transform.localScale += Vector3.one * Time.deltaTime * m_Speed;
+
+            // Calculate the new local scale for the child to maintain its world scale
+            Vector3 newLocalScale = new Vector3(
+                m_ChildInitialWorldScale.x / transform.localScale.x,
+                m_ChildInitialWorldScale.y / transform.localScale.y,
+                m_ChildInitialWorldScale.z / transform.localScale.z
+            );
+
+            // Apply the new local scale to the child
+            m_ChildTransform.localScale = newLocalScale;
+
+            KeepCentredOnFox();
+
+            yield return null;
+        }
+
+        // Set the parent scale to one at the end
+        transform.localScale = new Vector3(25,25,1);
+
+        // Reset child's local scale to its initial local scale
+        m_ChildTransform.localScale = m_ChildInitialWorldScale / 1.0f;
+    }
+
+    private void KeepCentredOnFox()
+    {
+        // Calculate the position of the UI element based on the object's position
+        Vector3 worldPosition = m_PlayerFox.transform.position;
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
+
+        // Set the position of the UI element's RectTransform
+        m_RectTransform.position = screenPosition;
     }
 }

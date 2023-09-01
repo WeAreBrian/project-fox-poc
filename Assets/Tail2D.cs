5f01d974dedefd213f9c73459533c0cd0314b1c4
Anjana <58893281+sachdawg@users.noreply.github.com>
2023-09-01 20:16:34 +1000

    fix(tail): finished tail stuffs

--- Assets/Tail2D.cs
+++ Assets/Tail2D.cs
@@ -4,62 +4,37 @@ using UnityEngine;
 
 public class Tail2D : MonoBehaviour
 {
-    private GameObject[] m_Children;
+	private GameObject[] m_Children;
 
-    [Header("Joint Settings")]
-    [SerializeField] private float m_JointAngleLimit = 45f;
-    [SerializeField] private float m_JointSpring = 500f;
-    [SerializeField] private float m_JointDamper = 10f;
+	//If something goes wrong in the future, animator should be the fox sprite with object with the animator in the scene. Target joint should be the last joint in the tail.
+	[SerializeField] private Animator m_FoxAnimator;
+	[SerializeField] private TargetJoint2D m_TargetJoint = null;
+	
+	private void Awake()
+	{
+		// Initialize the array with the children
+		m_Children = new GameObject[transform.childCount];
+		for (int m_Index = 0; m_Index < transform.childCount; m_Index++)
+		{
+			m_Children[m_Index] = transform.GetChild(m_Index).gameObject;
+		}
 
-    [Header("Rigidbody Settings")]
-    [SerializeField] private float m_RigidbodyMass = 1f;
+		UnparentAllChildren();
+	}
 
-    [SerializeField] private Animator m_Fox;
 
-    private void Awake()
-    {
-        // Initialize the array with the children
-        m_Children = new GameObject[transform.childCount];
-        for (int m_Index = 0; m_Index < transform.childCount; m_Index++)
-        {
-            m_Children[m_Index] = transform.GetChild(m_Index).gameObject;
-        }
-
-        UnparentAllChildren();
-
-        
+	//Make the targetjoints target be behind the player facing direction
+	private void FixedUpdate()
+	{
+		m_TargetJoint.target = m_FoxAnimator.transform.position + new Vector3(m_FoxAnimator.GetFloat("Facing") * -1000, m_FoxAnimator.transform.position.y);
     }
 
-    private void Update()
-    {
-        Debug.Log(m_Fox.GetFloat("Facing"));
-    }
-
-    private void UnparentAllChildren()
-    {
-        // Loop through all children and unparent them
-        foreach (GameObject m_Child in m_Children)
-        {
-            // Set the parent to null to unparent
-            m_Child.transform.SetParent(null);
-        }
-    }
-
-    private void ConfigureJointAndRigidbody()
-    {
-        foreach (GameObject m_Child in m_Children)
-        {
-            HingeJoint2D m_HingeJoint = m_Child.GetComponent<HingeJoint2D>();
-            if (m_HingeJoint != null)
-            {
-                
-            }
-
-            Rigidbody2D m_Rigidbody = m_Child.GetComponent<Rigidbody2D>();
-            if (m_Rigidbody != null)
-            {
-                
-            }
-        }
-    }
+	private void UnparentAllChildren()
+	{
+		// Loop through all children and unparent them
+		foreach (GameObject m_Child in m_Children)
+		{
+			m_Child.transform.SetParent(null);
+		}
+	}
 }

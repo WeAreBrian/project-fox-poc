e51ef1316c34cb7bd302913630ba77761a37ef96
ShiroKatake <tgkhang262@gmail.com>
2023-09-04 19:02:52 +1000

    feat: util functions for manipulating csv files

---
+++ Assets/Scripts/Utils/CsvUtils.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System;
+
+public static class CsvUtils
+{
+    private static string m_FullSavePath;
+
+    public static void CreateSaveFile()
+    {
+        // Create directory for the game (if there isn't one already)
+        string gamePath = @"My Games\A Foxs Tail";
+        string myDocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string fullPath = Path.Combine(myDocPath, gamePath);
+        Directory.CreateDirectory(fullPath);
+
+        // Create the save file at the specified path
+        string saveFileName = @"SpeedrunDatabase.csv";
+        m_FullSavePath = Path.Combine(fullPath, saveFileName);
+
+        using StreamWriter csv = new StreamWriter(m_FullSavePath);
+        csv.WriteLine("name,lv1Time,lv2Time,lv3Time,totalTime,email");
+    }
+
+    public static void WriteToFile(SpeedrunProfile profile)
+    {
+        using StreamWriter csv = new StreamWriter(m_FullSavePath, true);
+        csv.WriteLine($"{profile.PlayerName},{profile.Lv1Time},{profile.Lv2Time},{profile.Lv3Time},{profile.TotalTime},{profile.PlayerEmail}");
+    }
+}

06fa5e2e0fb68928c9f72b03236dbc22b5e94671
ShiroKatake <tgkhang262@gmail.com>
2023-09-04 13:05:08 +1000

    fix(time formatter): moved ms time up 1 digit

--- Assets/Scripts/Utils/TimeFormatter.cs
+++ Assets/Scripts/Utils/TimeFormatter.cs
@@ -4,7 +4,7 @@ public static class TimeFormatter
     {
         int minutes = (int)timeMillisecond / 60;
         int seconds = (int)timeMillisecond % 60;
-        int milliseconds = (int)(timeMillisecond * 100) % 100;
+        int milliseconds = (int)(timeMillisecond * 1000) % 1000;
 
         var timeText = string.Format("{0:00}:{1:00}.{2:000}", minutes, seconds, milliseconds);
 

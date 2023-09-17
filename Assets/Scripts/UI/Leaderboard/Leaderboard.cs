34e22c8aedb12401a213880231feeed2275c63b6
ShiroKatake <tgkhang262@gmail.com>
2023-09-17 13:32:58 +1000

    fix(leaderboard trophy visual): only show when there's a time to display

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -11,6 +11,10 @@ public class Leaderboard : MonoBehaviour
     private Transform m_TopProfileSpawn;
     [SerializeField]
     private Transform m_PlayerProfileSpawn;
+    [SerializeField]
+    private GameObject m_SilverTrophy;
+    [SerializeField]
+    private GameObject m_BronzeTrophy;
 
     private SpeedrunProfile m_PlayerProfile;
 
@@ -19,6 +23,15 @@ public class Leaderboard : MonoBehaviour
         // Load player profiles
         m_PlayerProfile = SaveUtils.GetPlayerData();
         List<SpeedrunProfile> orderedProfiles = SaveUtils.LoadProfiles();
+
+        if (orderedProfiles.Count < 2)
+        {
+            m_SilverTrophy.SetActive(false);
+        }
+        if (orderedProfiles.Count < 3)
+        {
+            m_BronzeTrophy.SetActive(false);
+        }
 
         // Show current player profile and their rank
         GameObject playerProfile = Instantiate(m_ProfilePrefab, m_PlayerProfileSpawn);

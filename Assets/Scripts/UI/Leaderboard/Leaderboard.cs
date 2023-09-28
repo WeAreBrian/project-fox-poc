8537e4e44ddc71a1461205ea27f244ebc5a73183
ShiroKatake <tgkhang262@gmail.com>
2023-09-28 18:07:27 +1000

    feat(input dialog): also update player name after submit

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -15,8 +15,24 @@ public class Leaderboard : MonoBehaviour
     private GameObject m_SilverTrophy;
     [SerializeField]
     private GameObject m_BronzeTrophy;
+    [SerializeField]
+    private InputDialog m_ProfileInputDialog;
+
+    private PlayerInputActions m_PlayerInputActions;
+    private List<SpeedrunStatTexts> m_SpeedrunStats = new List<SpeedrunStatTexts>();
 
     private SpeedrunProfile m_PlayerProfile;
+    private GameObject playerProfile;
+    private int m_PlayerRank;
+
+    private void Awake()
+    {
+        m_PlayerInputActions = new PlayerInputActions();
+
+        // Change to UI action maps (should be changed back in InputDialog.cs)
+        m_PlayerInputActions.Player.Disable();
+        m_PlayerInputActions.UI.Enable();
+    }
 
     private void Start()
     {
@@ -34,20 +50,41 @@ public class Leaderboard : MonoBehaviour
         }
 
         // Show current player profile and their rank
-        GameObject playerProfile = Instantiate(m_ProfilePrefab, m_PlayerProfileSpawn);
-        playerProfile.GetComponent<SpeedrunStatTexts>().Initialize(orderedProfiles.IndexOf(m_PlayerProfile) + 1, m_PlayerProfile);
+        m_PlayerRank = orderedProfiles.IndexOf(m_PlayerProfile) + 1;
+        playerProfile = Instantiate(m_ProfilePrefab, m_PlayerProfileSpawn);
+        playerProfile.GetComponent<SpeedrunStatTexts>().Initialize(m_PlayerRank, m_PlayerProfile);
 
         // Show other profiles
-        for (int i = 0; i < 10; i++) // Only show top 10 profiles
+        int loopLimit = Mathf.Min(orderedProfiles.Count, 10); // Only show top 10 profiles
+        for (int i = 0; i < loopLimit; i++)
         {
-            if (i >= orderedProfiles.Count)
-            {
-                break;
-            }
             Vector3 position = new Vector3(0, -i * m_ProfileTextPadding, 0);
-            GameObject stat = Instantiate(m_ProfilePrefab, m_TopProfileSpawn);
-            stat.GetComponent<SpeedrunStatTexts>().Initialize(i + 1, orderedProfiles[i]);
-            stat.GetComponent<RectTransform>().anchoredPosition = position;
+            GameObject statObject = Instantiate(m_ProfilePrefab, m_TopProfileSpawn);
+            statObject.GetComponent<RectTransform>().anchoredPosition = position;
+
+            SpeedrunStatTexts statText = statObject.GetComponent<SpeedrunStatTexts>();
+            statText.Initialize(i + 1, orderedProfiles[i]);
+            m_SpeedrunStats.Add(statText);
+        }
+
+        // Wait 3 seconds before showing dialog
+        LeanTween.delayedCall(3.0f, ActivateDialog);
+    }
+
+    private void ActivateDialog()
+    {
+        m_ProfileInputDialog.ActivateDialog();
+    }
+
+    public void UpdateProfileName()
+    {
+        m_PlayerProfile = SaveUtils.GetPlayerData();
+        playerProfile.GetComponent<SpeedrunStatTexts>().Initialize(m_PlayerRank, m_PlayerProfile);
+
+        // Also update the name in the leaderboard if the player is top 10
+        if (m_PlayerRank < 10)
+        {
+            m_SpeedrunStats[m_PlayerRank - 1].Initialize(m_PlayerRank, m_PlayerProfile);
         }
     }
 }

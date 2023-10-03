f9c0796f7d25b62063fc6018b88921f1f646b8ea
Evita Liu <evitaliu2001@gmail.com>
2023-10-03 15:27:52 +1100

    fix(inativity checker): added timer and show/hide ui

--- Assets/Scripts/InactivityChecker.cs
+++ Assets/Scripts/InactivityChecker.cs
@@ -2,40 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class InactivityChecker : MonoBehaviour
 {
-    private float m_lastTime;
+    private float m_CurrentResetTime;
+    private float m_CurrentInactiveTime;
+
     [SerializeField]
-    private float m_secondsUntilReset;
+    private float m_SecondsUntilReset;
     [SerializeField]
-    private int m_sceneIndexToLoad;
+    private float m_InactiveTimeLimit = 60f;
+    [SerializeField]
+    private int m_SceneIndexToLoad;
+    [SerializeField]
+    private TextMeshProUGUI m_Text;
+    [SerializeField]
+    private GameObject m_InactivityTimerContainer;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_lastTime = Time.fixedTime;
+        m_CurrentResetTime = m_SecondsUntilReset;
+        m_CurrentInactiveTime = m_InactiveTimeLimit;
+        m_InactivityTimerContainer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //detect any key and reset timer
-        if (Input.anyKey)
+        m_CurrentInactiveTime -= Time.deltaTime;
+
+        if (m_CurrentInactiveTime <= m_SecondsUntilReset)
         {
-            m_lastTime = Time.fixedTime;
+            m_InactivityTimerContainer.SetActive(true);
+            m_CurrentResetTime -= Time.deltaTime;
+            m_Text.text = Mathf.Round(m_CurrentResetTime).ToString();
+
+            //if timer is past x seconds, reset
+            if (m_CurrentInactiveTime <= 0f)
+            {
+                ResetGame();
+            }
         }
 
-        //if timer is past x seconds, reset
-        if(Time.fixedTime - m_lastTime > m_secondsUntilReset)
+        //if preses any key, reset timer
+        if (Input.anyKey)
         {
-            ResetGame();
+            RestTimer();
         }
     }
 
-    void ResetGame()
+    private void ResetGame()
     {
         SaveUtils.InitializeProfile();
-        SceneManager.LoadScene(m_sceneIndexToLoad);
-	}
+        SceneManager.LoadScene(m_SceneIndexToLoad);
+    }
+
+    private void RestTimer()
+    {
+        m_CurrentInactiveTime = m_InactiveTimeLimit;
+        m_CurrentResetTime = m_SecondsUntilReset;
+        m_InactivityTimerContainer.SetActive(false);
+    }
 }

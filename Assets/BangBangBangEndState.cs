0e1a93eeaf6bf2501d8a462bb100fae9bc8e916a
Brian Burns <brian.b5829@gmail.com>
2023-10-04 00:08:39 +1100

    more stuff

--- Assets/BangBangBangEndState.cs
+++ Assets/BangBangBangEndState.cs
@@ -9,7 +9,7 @@ public class BangBangBangEndState : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(3);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
